a365ebbf50bd981e663d24c4c76b6ab067801365
Stratmann <JStratma@sport1.de>
2017-04-12 19:07:08 +0200

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Router, Route, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
+
+//Load foundation-sites
+require('style!css!foundation-sites/dist/css/foundation.min.css')
+require('style!css!sass!applicationStyles')
+
+$(document).foundation();
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path='/' component={Main}>
+    </Route>
+  </Router>,
+  document.getElementById('app')
+);
---
+++ app/components/Main.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var Nav = require('Nav');
+
+var Main = (props) => {
+  return (
+    <div>
+      <Nav></Nav>
+      <p>Main.jsx rendered</p>
+    </div>
+  );
+}
+
+
+module.exports = Main;
