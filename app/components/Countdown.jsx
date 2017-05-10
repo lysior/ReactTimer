e3ab41a7775bd0e890da7e392d1b3206b67f2b10
Stratmann <JStratma@sport1.de>
2017-05-10 17:15:21 +0200

    Add countdown functionality

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -5,12 +5,33 @@ var CountdownForm = require('CountdownForm');
 
 var Countdown =  React.createClass({
   getInitialState: function() {
-    return {count: 0};
+    return {
+      count: 0,
+      countDownStatus: 'stopped'
+    };
   },
   handleSetCountdwon: function (seconds) {
     this.setState ({
-      count: seconds
+      count: seconds,
+      countDownStatus: 'started'
     });
+  },
+  componentDidUpdate: function (prevProps,prevState) {
+    if(this.state.countDownStatus !== prevState.countDownStatus) {
+      switch (this.state.countDownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  },
+  startTimer: function () {
+    this.timer = setInterval ( () => {
+      var newCount = this.state.count -1;
+      this.setState({
+        count: newCount >= 0 ? newCount: 0
+      });
+    },1000)
   },
   render: function () {
     var count = this.state.count;
---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,34 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var $ = require('jQuery');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+  it('should exist', () => {
+    expect(Countdown).toExist();
+  });
+
+  describe ('handleSetCountdwon', () => {
+    it('should be started and count down', () => {
+      var countdownObj = TestUtils.renderIntoDocument(<Countdown/>);
+      countdownObj.handleSetCountdwon(10);
+      expect(countdownObj.state.count).toBe(10);
+      expect(countdownObj.state.countDownStatus).toBe('started');
+
+      setTimeout( () => {
+        expect(countdownObj.state.count).toBe(9);
+      },1001);
+    });
+    it('should not be set to a negative number', () => {
+      var countdownObj = TestUtils.renderIntoDocument(<Countdown/>);
+      countdownObj.handleSetCountdwon(1);
+
+      setTimeout( () => {
+        expect(countdownObj.state.count).toBe(0);
+      },3001);
+    })
+  })
+})
